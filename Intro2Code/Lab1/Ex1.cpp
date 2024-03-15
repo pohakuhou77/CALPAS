76b74ff5f97125aa82a4dc981461ead6eb7a44d6
Kiet Cao Tuan <163552742+pohakuhou77@users.noreply.github.com>
2024-03-15 22:11:21 +0700

    Create Lab1 folder

---
+++ Intro2Code/Lab1/Ex1.cpp
@@ -0,0 +1,20 @@
+// MSSV: 21127328
+// Ten: Cao Tuan Kiet
+// Ex01: In kich thuoc kieu du lieu co ban 
+
+#include <iostream> 
+using namespace std; 
+
+int main() {
+	cout << "bool:" << sizeof(bool) << " bytes" << endl;
+	cout << "char:" << sizeof(char) << " bytes" << endl;
+	cout << "short:" << sizeof(short) << " bytes" << endl;
+	cout << "int:" << sizeof(int) << " bytes" << endl;
+	cout << "long:" << sizeof(long) << " bytes" << endl;
+	cout << "long long:" << sizeof(long long) << " bytes" << endl;
+	cout << "float:" << sizeof(float) << " bytes" << endl;
+	cout << "double:" << sizeof(double) << " bytes" << endl;
+	cout << "long double:" << sizeof(long double) << " bytes" << endl;
+
+	return 0; 
+}
